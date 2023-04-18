b729fb58f65897a887850a65230efba50fd5bd28
undertaker28 <pavel.starovoytovvv@gmail.com>
2023-04-18 16:54:21 +0300

    Add project structure & Camera Usage Description

---
+++ SnapList/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SnapList
+//
+//  Created by Pavel on 18.04.23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
